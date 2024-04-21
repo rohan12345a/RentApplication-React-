1d2d73ee720e7c99dfde377d9e63da10d0123066
Rohan Saraswat <rohan.saraswat2003@gmail.com>
2024-04-21 10:27:48 +0530

    Initial Commit

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './GetStarted.css'
+function GetStarted() {
+  return (
+    <section className="g-wrapper">
+        <div className="padding innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className='primaryText'>Get Started With RentEzi</span>
+                <span className='secondaryText'>Subscribe and help us to reach
+                <br /> 
+                Find your home soon
+                </span>
+                <button className="button">
+                    <a href="mailto:rohan.saraswat2003@gmail.com">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default GetStarted
